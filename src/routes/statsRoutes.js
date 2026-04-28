45b7ffefdade0eec14b5f5d18f7c6d86d2f18d7d
sudarshank264 <sudarshank264@gmail.com>
2026-04-27 23:31:31 -0700

    clean: remove leftover merge conflict comments

--- src/routes/statsRoutes.js
+++ src/routes/statsRoutes.js
@@ -1,16 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const { getDashboardStats } = require('../controllers/statsController');
-// const { protect } = require('../middleware/authMiddleware');
-
-// <<<<<<< HEAD
-// router.route('/')
-//     .get(protect, getDashboardStats);
-// =======
-// router.get('/', protect, getDashboardStats);
-// >>>>>>> 2a52a49 (doing backend fully)
-
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const { getDashboardStats } = require('../controllers/statsController');
